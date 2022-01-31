203a5dad385a7bbd6b1d1a68bdea17b8a9db48bf
tarkanbatar <tarkanbatar4@gmail.com>
2022-01-31 14:43:00 +0300

    fix: project structure fixed, run script added

---
+++ src/direct.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace DirectNotification;
+
+class Direct {
+    function sendNotification($to, $title, $message)
+    {
+        $msg = $message;
+        $content = array(
+            "en" => $msg
+        );
+        $headings = array(
+            "en" => $title
+        );
+        
+        $fields = array(
+            'app_id' => 'APP_ID',
+            "headings" => $headings,
+            'include_player_ids' => array($to),
+            'content_available' => true,
+            'contents' => $content,
+            "chrome_web_icon" => 'ICON_ID',
+            "chrome_web_image" => ' RESIM_URL'
+        );
+        
+        $headers = array(
+            'Authorization: key=API_KEY',
+            'Content-Type: application/json; charset=utf-8'
+        );
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://onesignal.com/api/v1/notifications');
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return $result;
+    }
+}
+
